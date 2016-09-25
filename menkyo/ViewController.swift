c2cc8332a85cd312dc8bfbdec9bae673aa3e4591
infosquare <infosquare@infosquare-no-macbook-pro.local>
2016-09-25 18:27:53 +0900

    Initial Commit

---
+++ menkyo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  menkyo
+//
+//  Created by infosquare on 2016/09/25.
+//  Copyright © 2016年 infosquare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
